2be7b4e71ddfc5bbcb37de4434d84ec940ca2e6a
mamjow <mj.moshiri@gmail.com>
2020-01-03 00:41:30 +0100

    PY-2018.2.3 <mjmos@DESKTOP-FP7QKLP

--- Management/views.py
+++ Management/views.py
@@ -5,11 +5,8 @@ from django.views.generic import TemplateView
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from .forms import AddPost
-
-from Homepage.models import BlogPost
-
 from django.shortcuts import get_object_or_404
-from Homepage.models import BlogPost, WebCategory
+from homepage.models import BlogPost, WebCategory
 
 User = get_user_model()
 
@@ -22,15 +19,64 @@ class Panel(TemplateView):
 
 
 def add_post(request):
+    if request.method == 'POST':
+        form = AddPost(request.POST or None, request.FILES or None)
+    else:
+        form = AddPost()
+    return save_post_form(request, form, 'manager/includes/newpost.html')
+
+
+def update_post(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+
+    if request.method == 'POST':
+        form = AddPost(request.POST, request.FILES, instance=post)
+    else:
+        form = AddPost(instance=post)
+    return save_post_form(request, form, 'manager/includes/updatepost.html')
+
+
+def delete_post(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
     data = dict()
-    form = AddPost()
-    context = {
-        'form': form
-    }
-    data['html_form'] = render_to_string('manager/newpost.html',
-                                         context,
-                                         request=request,
-                                         )
+    if request.method == 'POST':
+        post.delete()
+        data['form_is_valid'] = True  # This is just to play along with the existing code
+        posts = BlogPost.objects.all()
+        data['html_book_list'] = render_to_string('manager/table_post.html', {
+            'posts': post
+        })
+    else:
+        context = {'post': post}
+        data['html_form'] = render_to_string('manager/includes/deletepost.html',
+                                             context,
+                                             request=request,
+                                             )
+    return JsonResponse(data)
+
+
+def save_post_form(request, form, template_name):
+    data = dict()
+    if request.method == 'POST':
+        form = AddPost(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            posts = BlogPost.objects.all().order_by('post_time').reverse()
+            data['html_post_list'] = render_to_string('manager/includes/list_post.html', {
+                'posts': posts
+            })
+        else:
+            data['form_is_valid'] = False
+    context = {'form': form}
+    data['html_form'] = render_to_string(template_name, context, request=request)
+    return JsonResponse(data)
+
+
+def post_list(request):
+    posts = BlogPost.objects.all().order_by('post_time').reverse()
+    return render(request, 'manager/table_post.html', {'posts': posts})
+
     return JsonResponse(data)
 
 
--- management/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ModirConfig(AppConfig):
-    name = 'management'
