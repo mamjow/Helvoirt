a537e501c8e16fa2d36014309b563613deefdd21
mamjow <mj.moshiri@gmail.com>
2020-01-17 18:55:07 +0100

    Update views.py

--- Management/views.py
+++ Management/views.py
@@ -1,15 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import View
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, authenticate
 from django.views.generic import TemplateView
-from django.http import JsonResponse
-from django.template.loader import render_to_string
+from django.http import JsonResponse, HttpResponse
 from .forms import AddPost
-
-from Homepage.models import BlogPost
-
 from django.shortcuts import get_object_or_404
-from Homepage.models import BlogPost, WebCategory
+from homepage.models import BlogPost
 
 User = get_user_model()
 
@@ -18,34 +13,50 @@ User = get_user_model()
 
 
 class Panel(TemplateView):
-    template_name = 'manager/modiriat.html'
-
-
-def add_post(request):
-    data = dict()
-    form = AddPost()
-    context = {
-        'form': form
-    }
-    data['html_form'] = render_to_string('manager/newpost.html',
-                                         context,
-                                         request=request,
-                                         )
-    return JsonResponse(data)
-
-
-# def add_post(request):
-#
-#     inform = AddPost(request.POST or None, request.FILES or None)
-#     if request.method == 'POST' or None:
-#         inform.save()
-#     template = "manager/add_post.html"
-#     context = {
-#         'form': inform
-#     }
-#     return render(request, template, context)
+    template_name = 'manager/panel.html'
 
 
 def post_list(request):
-    posts = BlogPost.objects.all()
+    posts = BlogPost.objects.all().order_by('post_time').reverse()
     return render(request, 'manager/table_post.html', {'posts': posts})
+
+
+def update_post(request, pk):
+    obj = get_object_or_404(BlogPost, pk=pk)
+    if request.method == 'POST':
+        form = AddPost(request.POST or None, request.FILES or None, instance=obj)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.post_author = request.user
+            instance.save()
+            return redirect('/panel/posts/')
+    template = 'manager/includes/updatepost.html'
+    context = {
+        'form': AddPost(instance=obj),
+        'oid': pk,
+        'image': obj,
+    }
+    return render(request, template, context)
+
+
+def add_post(request):
+    if request.method == 'POST':
+        form = AddPost(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.post_author = request.user
+            instance.save()
+            return redirect('/panel/posts/')
+    template = 'manager/includes/newpost.html'
+    context = {
+        'form': AddPost,
+    }
+    return render(request, template, context)
+
+
+def delete_post(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+    data = ('success' + post.post_title)
+    return JsonResponse(data, safe=False)
